02ab2c66e32d21c94c2e18c3d6654784a5d7c7fb
David Paez <dpaez013@fiu.edu>
2020-10-03 04:10:52 -0400

    Changed the route to make homepage the starting page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from './home/home.component';
 import { NewRunComponent } from './new-run/new-run.component';
 
 const routes: Routes = [
+  {path: '', component: HomeComponent},
   {path: 'new-run', component: NewRunComponent},
   {path: 'home', component: HomeComponent}
 ];
