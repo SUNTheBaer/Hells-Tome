4b3c07d028f85e947d8fc9077fd7e7beb47df24c
SUNTheBaer <padron1999@yahoo.com>
2020-10-03 21:49:18 -0400

    added weapon images to new run page

--- src/app/new-run/new-run.component.ts
+++ src/app/new-run/new-run.component.ts
@@ -6,6 +6,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./new-run.component.css']
 })
 export class NewRunComponent implements OnInit {
+  weapons = [
+    {name: "Stygian Blade", img: "./assets/StygianBlade.png"},
+    {name: "Eternal Spear", img: "./assets/EternalSpear.png"},
+    {name: "Shield of Chaos", img: "./assets/ShieldOfChaos.png"},
+    {name: "Heart-Seeking Bow", img: "./assets/HeartSeekingBow.png"},
+    {name: "Twin Fists of Malphon", img: "./assets/TwinFistsOfMalphon.png"},
+    {name: "Adamant Rail", img: "./assets/AdamantRail.png"}
+  ]
 
   constructor() { }
 
